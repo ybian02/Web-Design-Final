f0774ca4ba6ad565be437ded40dc6d166b77a171
ybian02 <yuchun.bian@gmail.com>
2018-04-24 20:56:38 -0400

    April/24 home

--- js/index.js
+++ js/index.js
@@ -59,5 +59,22 @@ function addNextWord() {
     
   setTimeout(addNextWord, 80);
   
-  
 };
+
+// gif transition
+
+$(document).on(
+  {
+    mouseenter: function() {
+      var gifImage = $(this).find("img"),
+        gifImageSrc = gifImage.data("gif-image");
+      gifImage.attr("src", gifImageSrc);
+    },
+    mouseleave: function() {
+      var gifImage = $(this).find("img"),
+        coverSrc = gifImage.data("image");
+      gifImage.attr("src", coverSrc);
+    }
+  },
+  "[data-gif-trigger]"
+);
